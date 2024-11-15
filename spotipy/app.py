5b09bfb5e40129d804ee1fe979b01fc3be2069ff
Derien Weatherspoon <djamesw411@gmail.com>
2024-11-15 10:30:08 -0500

    add procfile and requirements

--- spotipy/app.py
+++ spotipy/app.py
@@ -47,8 +47,9 @@ def logout():
     scheduler.remove_all_jobs()  # Remove all scheduled jobs
     return "Logged out and scheduled updates removed."
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
 
 
 '''
